7ecf02a85f2b7d39f3247c456d13f7ac56db75ca
Saurabh Shukla <103saurabhshukla@gmail.com>
2024-02-15 02:54:52 +0530

    version 1 is completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import io from "socket.io-client";
 
 const socket = io("http://localhost:5000");
@@ -7,20 +7,66 @@ const socket = io("http://localhost:5000");
 const App = () => {
   const [username, setUserName] = useState("");
   const [chatActive, setChatActive] = useState(false);
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
+
+  useEffect(() => {
+    socket.on("received-message", (message) => {
+      setMessages([...messages, message]);
+    });
+  }, [messages, socket]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const messageData = {
+      message: newMessage,
+      user: username,
+      time:
+        new Date(Date.now()).getHours() +
+        ":" +
+        new Date(Date.now()).getMinutes(),
+    };
+
+    if (!newMessage == "") {
+      socket.emit("send-message", messageData);
+    } else {
+      alert("message cannot be empty");
+    }
+    
+    setNewMessage("");
+  };
 
   return (
     <>
       <div className="flex items-center justify-center w-screen h-screen bg-gray-100">
         {chatActive ? (
-          <div>
-            <h1>Squad Chat</h1>
+          <div className="w-full rounded-md md:w-[80vw] lg:w-[40vw] p-2">
+            <h1 className="my-2 text-xl font-bold text-center uppercase">Squad Chat</h1>
             <div>
-              <div></div>
-              <form className="flex gap-2 md:gap-4">
+              <div className="overflow-scroll h-[80vh] lg:h-[60vh]">
+                {messages.map((message, index) => {
+                  return (
+                    <div key={index} className={`flex my-5 rounded-md shadow-md w-fit ${username === message.user && "ml-auto" } `}>
+                      <div className="flex items-center justify-center bg-green-400 rounded-l-md">
+                        <h3 className="px-2 text-lg font-bold ">{message.user.charAt(0).toUpperCase()}</h3>
+                      </div>
+                      <div className="px-2 bg-white rounded-md">
+                        <span className="text-sm">{message.user}</span>
+                        <h3 className="font-bold">{message.message}</h3>
+                        <h3 className="text-xs text-right">{message.time}</h3>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+              <form className="flex justify-between gap-2 md:gap-4" onSubmit={handleSubmit}>
                 <input
                   type="text"
+                  value={newMessage}
                   placeholder="Type your message ..."
-                  className="px-3 py-2 border-2 rounded-md outline-none"
+                  className="w-full rounded-md outline-none bofrder-2 p-y-2 "
+                  onChange={(e) => setNewMessage(e.target.value)}
                 />
                 <button
                   type="text"
--- server/index.js
+++ server/index.js
@@ -1,32 +1,35 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
-const cors = require('cors');
+const http = require("http");
+const cors = require("cors");
 const port = 5000;
-const {Server} = require("socket.io");
+const { Server } = require("socket.io");
 
 app.use(cors());
 
-
 const server = http.createServer(app);
 
-const io = new Server(server,{
-    cors: {
-        origin: "http://localhost:5173",
-        methods: ["GET","POST"]
-    }
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
 });
 
+io.on("connection", (socket) => {
+  console.log(`User connected : ${socket.id}`);
 
+  socket.on("send-message", (message) => {
+    
+    // Broadcast the message to all the connected users
+    io.emit("received-message", message);
+  });
 
-io.on("connection", (socket)=>{
-    console.log(`User connected : ${socket.id}`);
-
-    socket.on("disconnect", () => {
-        console.log("User Disconnected");
-    });
+  socket.on("disconnect", () => {
+    console.log("User Disconnected");
+  });
 });
 
 server.listen(port, () => {
-    console.log("server is up at port: ",port);
-})
+  console.log("server is up at port: ", port);
+});
